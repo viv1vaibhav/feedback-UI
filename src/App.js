28429b9ea7f5ebdc7f2570e9ecf4cf6a5b0e8305
Vivek Vaibhav <t-vvaibhav@expediagroup.com>
2023-06-15 14:57:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./components/context/FeedbackContext";
+
+function App() {
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
+}
+
+export default App;
